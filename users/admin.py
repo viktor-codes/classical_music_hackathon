18728805058bac79e9fef2a7c9bb7a0c20a139d5
Viktor Rura <ruravictor@gmail.com>
2024-09-20 09:26:55 +0100

    Register user in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("email", "is_staff", "is_active")
