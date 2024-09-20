016d238561ad96c42bbdc1a0c3a2546b23a9819e
Viktor Rura <ruravictor@gmail.com>
2024-09-20 10:43:51 +0100

    Add header and footer

--- music_hackathon/settings.py
+++ music_hackathon/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "users",
     "tailwind",
     "theme",
+    "django_browser_reload",
 ]
 
 TAILWIND_APP_NAME = "theme"
@@ -65,6 +66,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "music_hackathon.urls"
@@ -133,7 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+MEDIA = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
