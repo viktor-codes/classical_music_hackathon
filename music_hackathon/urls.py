b27a48fda925e50b67cc462320db86a42d133c1f
Leonie McLaughlin <126171574+leoniemclaughlin7@users.noreply.github.com>
2024-09-20 21:22:30 +0000

    Revert "Merge pull request #3 from leoniemclaughlin7/authentication"
    This reverts commit 5423cbfe8b887c7776547aafdc8391142da64ded, reversing
    changes made to 9d31e918f38207eecf4916f8f7de142dc0561c6f.
    
    Revert back to commit 9d31e918f38207eecf4916f8f7de142dc0561c6f

--- music_hackathon/urls.py
+++ music_hackathon/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("users.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
-    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
