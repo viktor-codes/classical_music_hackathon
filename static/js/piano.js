aa327e370cb827f63b9fa0d47d0ca47ac34f9924
MiaRasmussen05 <rasmussenm376@gmail.com>
2024-09-22 21:58:07 +0000

    Fix lesson css with piano volume and start on showing notes

--- static/js/piano.js
+++ static/js/piano.js
@@ -1,17 +1,19 @@
 const note = document.querySelector(".nowplaying");
+const volume = document.querySelector(".volume-range input");
 const keys = document.querySelectorAll(".key"),
   hints = document.querySelectorAll(".hints"),
   hintButton = document.querySelector(".hint-btn"),
-  keysContainer = document.querySelector(".keys")
+  keysContainer = document.querySelector(".keys"),
+  imageElement = document.querySelector(".music-wrapper img");
   
 // Function to play notes based on key press or click
 function playNoteFromKey(e) {
   let keyCode;
 
   if (e.type === 'keydown') {
-    keyCode = e.keyCode;
+      keyCode = e.keyCode;
   } else if (e.type === 'click') {
-    keyCode = e.currentTarget.getAttribute("data-key");
+      keyCode = e.currentTarget.getAttribute("data-key");
   }
 
   const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
@@ -25,7 +27,42 @@ function playNoteFromKey(e) {
   audio.currentTime = 0;
   audio.play();
 
+  const images = {
+    "65": "../static/images/notes/c-1.png",
+    "87": "{% static 'images/notes/c-1.png' %}", 
+    "83": "images/notes/d-1.png",
+    "69": "images/notes/d-sharp-1.png",
+    "68": "images/notes/e-1.png",
+    "70": "images/notes/f-1.png",
+    "84": "images/notes/f-sharp-1.png",
+    "71": "images/notes/g-1.png",
+    "89": "images/notes/g-sharp-1.png",
+    "72": "images/notes/a-1.png",
+    "85": "images/notes/b-1.png",
+    "74": "images/notes/b-flat-1.png",
+    "75": "images/notes/c-2.png",
+    "79": "images/notes/c-sharp-2.png",
+    "76": "images/notes/d-2.png",
+    "80": "images/notes/d-sharp-2.png",
+    "186": "images/notes/e-2.png"
+  };
+
+  // Update the image source based on the key pressed
+  if (images[keyCode]) {
+      imageElement.src = images[keyCode];
+      imageElement.style.display = 'block'; // Show the image
+  } else {
+      imageElement.style.display = 'none'; // Hide if no matching image
+  }
 }
+
+// Volume control handler volume
+const handleVolume = (e) => {
+  const audioElements = document.querySelectorAll("audio");
+  audioElements.forEach(audio => {
+    audio.volume = e.target.value; // Set the volume for each audio element
+  });
+};
 
 // Remove playing class after transition ends
 function removeTransition(e) {
@@ -59,6 +96,7 @@ function toggleHints() {
   }
 }
 
+volume.addEventListener("input", handleVolume);
 hintButton.addEventListener("click", toggleHints);
 
 hints.forEach((hint, index) => {
