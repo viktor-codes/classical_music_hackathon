bdac6a23095a4e325e4bd3f71b250128c7639537
MiaRasmussen05 <rasmussenm376@gmail.com>
2024-09-23 05:14:57 +0000

    Add highscore page, add new items for the game, fixed path ways for highscores and a lot of styling

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("team/", views.team, name="team"),
+    path("highscore/", views.highscore, name="highscore"),
     path("register/", views.SignUpView.as_view(), name="register"),
 ]
